894562a9d5df38703c99cfdbb849900efaae2e65
BassirouD <moubass.diallo@gmail.com>
2023-04-28 23:59:25 +0200

    first commit

---
+++ src/main/java/diallo/org/sprint_unittest/SprintUnitTestApplication.java
@@ -0,0 +1,13 @@
+package diallo.org.sprint_unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintUnitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/diallo/org/sprint_unittest/SprintUnitTestApplicationTests.java
@@ -0,0 +1,28 @@
+package diallo.org.sprint_unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class SprintUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void assertThanOnePlusOneIsTwo() {
+        int somme = 1 + 1;
+        assertThat(somme).isNotNull();
+        assertThat(somme).isEqualTo(2);
+    }
+
+    @Test
+    void assertThanOnePlusTwoIsThree() {
+        int somme = 1 + 2;
+        assertThat(somme).isEqualTo(3);
+    }
+
+}
